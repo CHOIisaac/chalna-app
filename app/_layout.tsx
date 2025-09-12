ce21a94894ce577d909c9c5085ed9ed130dad2ec
CHOIisaac <ehdrhkdls@gmail.com>
2025-09-12 22:19:56 +0900

    refactor: _layout.tsx에서 라우트명 변경

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout() {
           <Stack.Screen name="edit-field" options={{ headerShown: false }} />
           <Stack.Screen name="add-ledger" options={{ headerShown: false }} />
           <Stack.Screen name="add-event" options={{ headerShown: false }} />
-          <Stack.Screen name="event-detail" options={{ headerShown: false }} />
-          <Stack.Screen name="edit-event-field" options={{ headerShown: false }} />
+          <Stack.Screen name="schedule-detail" options={{ headerShown: false }} />
+          <Stack.Screen name="edit-schedule-field" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
       <StatusBar style="dark" />
