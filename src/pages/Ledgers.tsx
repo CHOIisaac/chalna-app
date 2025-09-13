7a8ea9f157fc3cddd65fff989069e7841f78621e
CHOIisaac <ehdrhkdls@gmail.com>
2025-09-13 22:01:00 +0900

    style: 목록 카드 marginBottom 8로 변경

--- src/pages/Ledgers.tsx
+++ src/pages/Ledgers.tsx
@@ -628,7 +628,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    marginBottom: 6,
+    marginBottom: 8,
   },
   ledgerName: {
     fontSize: 16,
